b9ab806b758335cf3a2d9f549128f9d6d4818e21
garcisanti <garciasantiago10@gmail.com>
2017-04-07 11:59:22 -0500

    business main

---
+++ src/app/business-main/business-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-business-main',
+  templateUrl: './business-main.component.html',
+  styleUrls: ['./business-main.component.css']
+})
+export class BusinessMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
